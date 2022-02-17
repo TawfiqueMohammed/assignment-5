80a23d5ea0c90783d046705c7a48fa081cf53de7
Tawfique Mohammed <tawfiquemt17@gmail.com>
2022-02-17 16:22:06 +0600

    comment added

--- js/money.js
+++ js/money.js
@@ -1,16 +1,20 @@
 document.getElementById('calculate-button').addEventListener('click', function () {
+    // defining income input
     const incomeInput = document.getElementById('income-input');
     const newIncomeInputText = incomeInput.value;
     const newIncomeInput = parseFloat(newIncomeInputText);
 
+    // defining food input
     const foodInput = document.getElementById('food-input');
     const newFoodInputText = foodInput.value;
     const newFoodInput = parseFloat(newFoodInputText);
 
+    // defining rent input
     const rentInput = document.getElementById('rent-input');
     const newRentInputText = rentInput.value;
     const newRentInput = parseFloat(newRentInputText);
 
+    // defining cloth input
     const clothInput = document.getElementById('cloth-input');
     const newClothInputText = clothInput.value;
     const newClothInput = parseFloat(newClothInputText);
@@ -33,9 +37,24 @@ document.getElementById('calculate-button').addEventListener('click', function (
     const balance = newIncomeInput - newTotalExpenses;
     totalBalance.innerText = balance;
 
+});
+
+document.getElementById('save-button').addEventListener('click', function () {
+    // defining income input
+    const incomeInput = document.getElementById('income-input');
+    const newIncomeInputText = incomeInput.value;
+    const newIncomeInput = parseFloat(newIncomeInputText);
+
     // save part
-    const saveInput = document.getElementById('save');
+    const saveInput = document.getElementById('save-input');
     const newSaveInputText = saveInput.value;
     const newSaveInput = parseFloat(newSaveInputText);
 
+    const savingAmount = document.getElementById('saving-amount');
+    const previousSavingAmountText = savingAmount.innerText;
+    // const previousSavingAmount = parseFloat(previousSaveInputText);
+
+    const newSavingAmount = (newIncomeInput * newSaveInput) / 100;
+    console.log(newSavingAmount);
+    savingAmount.innerText = newSavingAmount;
 });
